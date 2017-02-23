ffbc84cbe0afc014e9a481c986ae08eceb8cdc47
Jason Butwell <jasonbutwell@gmail.com>
2017-02-23 02:27:18 +0000

    List items are now shown in a GridView using a Grid Layout Manager. Also implemented ability to cancel old Toast message when displaying a new one, when clicking on a list item.

--- app/src/main/java/com/jasonbutwell/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/recyclerviewexample/MainActivity.java
@@ -2,15 +2,20 @@ package com.jasonbutwell.recyclerviewexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements CustomRecyclerViewAdapter.ListItemClickListener {
 
+    private static final int NUMBER_OF_ITEMS_IN_GRID_ROW = 2;
+
     // Class variables for the RecyclerView and the adapter
     private CustomRecyclerViewAdapter mAdapter;
     private RecyclerView mList;
+
+    // Class variable to store current Toast message so we can cancel it with a new one
+    private static Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +30,11 @@ public class MainActivity extends AppCompatActivity implements CustomRecyclerVie
 
         // Setup the RecyclerView
         mList = (RecyclerView) findViewById(R.id.recyclerView);
-        mList.setLayoutManager(new LinearLayoutManager(this));
+
+        //mList.setLayoutManager(new LinearLayoutManager(this));
+
+        // Grid Layout
+        mList.setLayoutManager(new GridLayoutManager(this,NUMBER_OF_ITEMS_IN_GRID_ROW));
 
         // Used to improve performance
         mList.setHasFixedSize(true);
@@ -38,8 +47,16 @@ public class MainActivity extends AppCompatActivity implements CustomRecyclerVie
     // What we want to happen when a recyclerview list item is clicked
     @Override
     public void onListItemClick(int clickedItemIndex) {
+
+        // If there are any other toast messages existing, cancel them
+        if ( mToast != null )
+            mToast.cancel();
+
         // Just display a toast message with the index of the item clicked on for now.
         String message = "You clicked on item: "+String.valueOf(clickedItemIndex);
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        // Create new Toast message and display it
+        mToast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        mToast.show();
     }
 }
